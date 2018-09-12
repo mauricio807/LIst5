488f6d24481c7bcedad14257b2226562e3236260
mauricio807 <38573479+mauricio807@users.noreply.github.com>
2018-09-12 18:43:17 -0300

    Add files via upload

---
+++ TestPessoa.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Quest2P;
+
+/**
+ *
+ * @author stranger
+ */
+public class TestPessoa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+          Rico r = new Rico();
+       Pobre p = new Pobre();
+       PobreLascado pl = new PobreLascado();
+       
+       
+       r.torraGrana();
+        
+        p.trabalhador();
+        
+        pl.Mendinga();
+        
+}
+    }
+    
+
