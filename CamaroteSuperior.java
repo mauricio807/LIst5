a74f8d5bf4597f4a41ac54b21fda95c7e31c100e
mauricio807 <38573479+mauricio807@users.noreply.github.com>
2018-09-12 17:52:42 -0300

    Update CamaroteSuperior.java

--- CamaroteSuperior.java
+++ CamaroteSuperior.java
@@ -10,5 +10,20 @@ package Quest3;
  * @author stranger
  */
 public class CamaroteSuperior {
+  private float caro;
+    
+    public float getValor(){
+        return Valor;
+    }
+    
+    public void setValor(float valor){
+        this.Valor = Valor;
+    }
+    
+   @Override
+   public void imprimevalor(){
+       float valor = (getPreco() + getValorAdicional() + 70);
+        JoptionPane.showMessageDialog("O ingresso pra Area Vip custa: " + valor);
+    }
     
 }
